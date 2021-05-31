78010f40ca07ac87fcd3f109b97e180ee8d52e3f
Mohammed Ahmed <waheedahmed14@gmail.com>
2021-05-31 22:17:01 +0100

    images download automatically

--- wallpaper.py
+++ wallpaper.py
@@ -1,18 +1,24 @@
 import requests
-import config
+# import config
 import json
+import urllib.request
+import datetime
 
-headers = {"Authorization": "bearer "+config.client_id, "User-Agent": "ChangeMeClinet/0.1 by YourUsername"}
-#makes a call to the reddit website, specifically to the top pots in the wallpaper subreddit
-response = requests.get("https://oauth.reddit.com/r/wallpapers/top", headers=headers)
+# headers = {"Authorization": "bearer "+config.client_id, "User-Agent": "ChangeMeClinet/0.1 by YourUsername"}
+# #makes a call to the reddit website, specifically to the top pots in the wallpaper subreddit
+# response = requests.get("https://oauth.reddit.com/r/wallpapers/top", headers=headers)
 
-#posts results from this call to the below file
+# #posts results from this call to the below file
 filename = 'results.json'
-with open(filename, 'w') as f:
-    json.dump(response.json(), f, indent=4)
+# with open(filename, 'w') as f:
+#     json.dump(response.json(), f, indent=4)
 
 #from this file the data is read and the url file is grabbed
 with open(filename, 'r') as f:
    data = f.read()
 posts = json.loads(data)
 print(posts["data"]["children"][0]["data"]["url_overridden_by_dest"])
+url = posts["data"]["children"][0]["data"]["url_overridden_by_dest"]
+r = urllib.request.urlopen(url)
+with open(str(datetime.date.today()) + ".png", "wb") as f:
+    f.write(r.read())
